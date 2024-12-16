c9f36a84518dd93122eeb9761fcb85f64c345e26
gitmore21 <142077387+gitmore21@users.noreply.github.com>
2024-12-16 16:16:58 -0300

    Update download.py

--- download.py
+++ download.py
@@ -9,13 +9,19 @@ def download_from_google_drive(file_id, output_path):
       output_path: The path where the file should be saved.
   """
   try:
-    gdown.download(id=file_id, output=output_path, quiet=False)
+    url = f'https://drive.google.com/uc?id={file_id}'
+    gdown.download(url, output_path, quiet=False, fuzzy=True) # Use fuzzy=True for large files
     print(f"File downloaded successfully to: {output_path}")
   except Exception as e:
     print(f"Error downloading file: {e}")
+    exit(1)  # Exit with an error code if download fails
 
 if __name__ == "__main__":
-  file_id = os.getenv("1mbP66SZCS0jK7DKeQKb3PcvdgtmDdDNq")
+  file_id = os.getenv("GOOGLE_DRIVE_FILE_ID")
+  if not file_id:
+    print("Error: GOOGLE_DRIVE_FILE_ID environment variable not set.")
+    exit(1)
+
   output_filename = "Server.zip"
   output_directory = "."
 
